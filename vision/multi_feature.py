68d3e8f6b468a14ef7cd292a254e08380ea79fb0
Gourav Singh Rajput <gsrajput24sept@gmail.com>
2026-06-13 15:15:18 +0530

    fingers added

--- vision/multi_feature.py
+++ vision/multi_feature.py
@@ -34,4 +34,62 @@ def multi_feature():
 
     while True:
         ret, frame = cap.read()
-        
+
+        if not ret:
+            print("cannot receive the frame")
+            break 
+
+        frame = cv.flip(frame, 1)
+        frame = cv.resize(frame, (1000, 700))
+        # frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+        results = hands.process(frame)
+
+        h, w, _ = frame.shape 
+
+        right_index = None
+        right_thumb = None 
+        left_index = None
+        left_thumb = None 
+
+
+        left_pinch = False
+        right_pinch = False
+
+        if results.multi_hand_landmarks and results.multi_handedness:
+            for hand_landmarks, hand in zip(
+                results.multi_hand_landmarks, results.multi_handeddness
+            ):
+
+                hand_label = hand.classification[0].label
+
+                thumb_tip_landmark = hand_landmarks.landmark[4]
+                index_tip_landmark = hand_landmarks.landmark[8]
+                middle_tip_landmark = hand_landmarks.landmark[12]
+                ring_tip_landmarks = hand_landmarks.landmark[16]
+                pinky_tip_landmarks = hand_landmarks.landmark[20]
+
+                thumb_tip_coords = (
+                    int(thumb_tip_landmark.x * w),
+                    int(thumb_tip_landmark.y * h)
+                )
+
+                index_tip_coords = (
+                    int(index_tip_landmark.x * w),
+                    int(index_tip_landmark.y * h)
+                )
+
+                middle_tip_coords = (
+                    int(middle_tip_landmark.x * w),
+                    int(middle_tip_landmark.y * h)
+                )
+
+                ring_tip_coords = (
+                    int(ring_tip_landmarks.x * w),
+                    int(ring_tip_landmarks.y * h)
+                )
+
+                pinky_tip_coords = (    
+                    int(pinky_tip_landmarks.x * w),
+                    int(pinky_tip_landmarks.y * h)
+                )   
