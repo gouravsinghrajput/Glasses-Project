96084a38c423107e68ce07ccc7946229243d4d5a
Gourav Singh Rajput <gsrajput24sept@gmail.com>
2026-09-11 19:10:38 +0530

    structure for the recording

--- audio/open_camera.py
+++ audio/open_camera.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import time 
-import pyaudio 
+import pyaudio
 
 
 from openwakeword.model import Model 
@@ -9,10 +9,54 @@ from faster_whisper import WhisperModel
 
 RATE = 16000
 CHUNK = 1280 
+RECORD_TIME = 4
 
 
 WAKE_THRESHOLD = 0.5 
 COMMAND_TIME = 5 
 
 
+COMMANDS = ["open camera", 
+            "open the camera", 
+            "can you please open the camera for me",
+            "start the camera", 
+            "turn on the camera", 
+            "activate the camera", 
+            "activate camera"
+            ]
 
+
+
+model = WhisperModel(
+    "small", 
+    device = 'cpu', 
+    compute_type = 'int8'
+)
+
+
+audio_input = pyaudio.PyAudio()
+
+
+stream = audio_input.open(
+    format = pyaudio.paInt16, 
+    channels = 1, 
+    rate = RATE, 
+    input = True, 
+    frames_per_buffer = CHUNK
+)
+
+#this printing is just for the testing
+print ('listning...')
+
+frames = []
+
+
+# the loop variable doesn't matter here
+#the number of iteration doesn't matter here
+#what matters is whether the complete thing is iterated or not
+
+
+# the iteration will go from basically 0 to RATE / CHUNK * RECORD_TIME
+#this is the number of frames that will be procesed in a sec
+for i in range (int(RATE / CHUNK * RECORD_TIME)):
+    
