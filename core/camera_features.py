6f46a0a8e7bd7a985a0de4567e003539e0161219
Gourav Singh Rajput <gsrajput24sept@gmail.com>
2026-09-12 15:30:43 +0530

    basic call function working

--- core/camera_features.py
+++ core/camera_features.py
@@ -1,9 +1,13 @@
-from audio.open_camera import open_camera_audio_call 
+from audio.open_camera import open_camera_audio_call, COMMANDS, BREAK_WORD
 from vision.camera_opening import camera_opening
 
 
-if open_camera_audio_call.spoken_text in [command.lower() for command in open_camera_audio_call.COMMANDS]:
-    camera_opening()
+spoken_text = open_camera_audio_call() 
+
+if spoken_text in [command.lower() for command in COMMANDS]:
+
+    camera_opening() 
 
 else:
-    print('retry')
+
+    print ("retry")
