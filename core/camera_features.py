d051d07b2e1654a77841d7b7e61566491d950f45
Gourav Singh Rajput <gsrajput24sept@gmail.com>
2026-09-12 15:38:58 +0530

    adding the break function

--- core/camera_features.py
+++ core/camera_features.py
@@ -2,12 +2,18 @@ from audio.open_camera import open_camera_audio_call, COMMANDS, BREAK_WORD
 from vision.camera_opening import camera_opening
 
 
-spoken_text = open_camera_audio_call() 
+while True:
 
-if spoken_text in [command.lower() for command in COMMANDS]:
+    spoken_text = open_camera_audio_call() 
 
-    camera_opening() 
+    if spoken_text in [command.lower() for command in COMMANDS]:
 
-else:
+        camera_opening() 
 
-    print ("retry")
+    else:
+
+        print ("retry")
+
+    if spoken_text in [break_word.lower() for break_word in BREAK_WORD]:
+
+        break
