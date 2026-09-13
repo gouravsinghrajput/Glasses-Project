a52038c917a3cfd56c78127be7c955f86b86c901
Gourav Singh Rajput <gsrajput24sept@gmail.com>
2026-09-13 18:08:15 +0530

    scrapping everything, and maybe we will use threading or completly different structure this time

--- core/camera_features.py
+++ core/camera_features.py
@@ -4,27 +4,3 @@ import cv2 as cv
 
 
 
-while True:
-
-    spoken_text  = open_camera_audio_call()
-
-    if spoken_text.lower() in [command.lower() for command in COMMANDS]:
-
-        camera_opening() = True 
-
-        while True:
-
-            spoken_text = open_camera_audio_call()
-
-            cap = camera_opening()
-
-            if spoken_text.lower() in [break_word.lower() for break_word in BREAK_WORD]:
-
-
-                cap.release()    
-                cv.destroyAllWindows()
-
-
-    else:
-
-        print('retry')          
