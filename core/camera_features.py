30f69166911c07e137e4f36cd21d2aa7c88c48d5
Gourav Singh Rajput <gsrajput24sept@gmail.com>
2026-09-13 17:15:48 +0530

    opening ther camera

--- core/camera_features.py
+++ core/camera_features.py
@@ -2,18 +2,19 @@ from audio.open_camera import open_camera_audio_call, COMMANDS, BREAK_WORD
 from vision.camera_opening import camera_opening
 
 
+spoken_text = open_camera_audio_call() 
+
+
 while True:
 
-    spoken_text = open_camera_audio_call() 
-
     if spoken_text in [command.lower() for command in COMMANDS]:
-
         camera_opening() 
 
-    else:
+        while camera_opening():
 
-        print ("retry")
+            if spoken_text in [break_word.lower() for break_word in BREAK_WORD]:
+                break
 
-    if spoken_text in [break_word.lower() for break_word in BREAK_WORD]:
+    else: 
+        print('retry')
 
-        break
