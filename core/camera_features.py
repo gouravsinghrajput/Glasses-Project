155bc97033cdd21d1f88df768127f1c1eaaa2a80
Gourav Singh Rajput <gsrajput24sept@gmail.com>
2026-09-13 18:11:18 +0530

    organizing

--- core/camera_features.py
+++ core/camera_features.py
@@ -1,4 +1,4 @@
-from audio.open_camera import open_camera_audio_call, COMMANDS, BREAK_WORD
+from audio.open_camera_audio_file import open_camera_audio_call, COMMANDS, BREAK_WORD
 from vision.camera_opening import camera_opening
 import cv2 as cv 
 
